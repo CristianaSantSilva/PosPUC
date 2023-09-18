25246de5c8121509d40777be6268ef07a20d2f28
CEMIG\cristiana.silva <cristiana.silva@cemig.com.br>
2023-09-17 22:03:37 -0300

    Trabalho Node.js API REST - Banco de Dados

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -14,16 +14,6 @@ const knex = require('knex')
 apiRouter.use (express.json())
 const endpoint = '/'
 
-const lista_produtos = {
-    produtos: [
-        { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João"  },
-        { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans"  },
-        { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé"  },
-        { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps"  },
-        { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé"  },
-    ]
-}
-
 //Retorna um json com todos os cursos cadastrados
 apiRouter.get(endpoint + 'cursos', (req, res) => {
         knex('cursos').then(cursos => { 
